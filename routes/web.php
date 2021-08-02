56c8ba0e266b6cf83284394ecbae99b5d2b6d270
sabbirshawon <sabbirs93@gmail.com>
2021-08-03 03:02:34 +0600

    product api files using lumen microservice

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+
+$router->group(['prefix'=>'api/v1'], function() use($router){
+
+    $router->get('/items', 'ProductController@index');
+    $router->post('/items', 'ProductController@create');
+    $router->get('/items/{id}', 'ProductController@show');
+    $router->put('/items/{id}', 'ProductController@update');
+    $router->delete('/items/{id}', 'ProductController@destroy');
+
+});
